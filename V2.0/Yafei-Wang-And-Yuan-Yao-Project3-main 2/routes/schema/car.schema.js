9aacb9280753d9cbee78806535ff466ec679e052
xiaochenma1996 <ma.xiaoch@northeastern.edu>
2022-07-09 20:51:20 -0700

    Adding car schema for car model

---
+++ V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/routes/schema/car.schema.js
@@ -0,0 +1,58 @@
+const Schema = require('mongoose').Schema;
+
+const carSchema = new Schema({
+    username: String,
+    title: {
+        required: true,
+        type: String
+    },
+    Vin: {
+        type: String
+    },
+    Year: {
+        type: Number
+    },
+    Make: {
+        type: String
+    },
+    Model: {
+        type: String
+    },
+    Trim: {
+        type: String
+    },
+    Body: {
+        type: String
+    },
+    Transmission: {
+        type: String
+    },
+    State: {
+        type: String
+    },
+    Odometer: {
+        type: Number
+    },
+    CarCondition: {
+        type: Number
+    },
+    Color: {
+        type: String
+    },
+    Interior: {
+        type: String
+    },
+    Mmr: {
+        type: Number
+    },
+    SellingPrize: {
+        type: Number
+    },
+    SellerId: {
+        type: Number
+    }
+}, {
+    collection: 'car',
+})
+
+module.exports = carSchema;
