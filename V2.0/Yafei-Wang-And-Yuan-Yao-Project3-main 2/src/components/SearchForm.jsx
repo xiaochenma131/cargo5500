9fe777088b44d1eeed22cc2977886a447f131768
xiaochenma1996 <ma.xiaoch@northeastern.edu>
2022-07-20 21:36:24 -0700

    modifying styles

--- V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/src/components/SearchForm.jsx
+++ V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/src/components/SearchForm.jsx
@@ -34,38 +34,19 @@ export default function SearchForm(props) {
     useEffect(getCars, []);
     return (
         <div>
-            {/* <form className="search-form">
-                <label>
-                    Year:{' '}
-                    <input type="text" name="Year" value={Year} onChange={e => setYear(e.target.value)} />
-                </label> */}
-            {/* <label>
-                    Make:{' '}
-                    <input type="text" name="Make" value={Make} onChange={e => setMake(e.target.value)} />
-                </label>
-                <label>
-                    Model:{' '}
-                    <input type="text" name="Model" value={Model} onChange={e => setModel(e.target.value)} />
-                </label>
-                <label>
-                    State:{' '}
-                    <input type="text" name="State" value={State} onChange={e => setState(e.target.value)} />
-                </label> */}
-            {/* <input class="search-form-button" type="submit" value="Search" onClick={getCars()} /> */}
-            {/* <input class="search-form-button" type="submit" value="Search" /> */}
-            {/* <button class="search-form-button" type="submit" onClick={() => getCars()}>Search</button>
-            </form > */}
-            <Card className='w-auto'>
-                <Card.Header>Search Cars</Card.Header>
-                <Card.Body>
-                    <h5>Year</h5>
-                    <input value={Year} onChange={e => setYear(e.target.value)} />
-                </Card.Body>
-            </Card>
-            <Button size="lg" className="custom-btn mt-3" onClick={() => getCars()} >
-                Search
-            </Button>
+            <div className="car-card">
+                <Card className='w-auto'>
+                    <Card.Header>Search Cars</Card.Header>
+                    <Card.Body>
+                        <h5>Year</h5>
+                        <input value={Year} onChange={e => setYear(e.target.value)} />
+                    </Card.Body>
+                </Card>
+                <Button size="lg" className="custom-btn mt-3" onClick={() => getCars()} >
+                    Search
+                </Button>
+            </div>
             <CarCards cars={cars} />
-        </div >
+        </div>
     )
 }
