2ad65e34d7300c2c3a6e2b54a92a241a5b8537d2
xiaochenma1996 <ma.xiaoch@northeastern.edu>
2022-07-26 23:45:30 -0700

    Allow customers to pass in optional amount of filters while searching cars

--- V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/routes/model/car.model.js
+++ V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/routes/model/car.model.js
@@ -17,12 +17,86 @@ function deleteCar(car) {
 }
 
 function getCarsByFilter(year, make, model, state) {
-    return CarModel.find({
-        Year: year,
-        Make: make,
-        Model: model,
-        State: state,
-    }).exec();
+    if (year != '' && make != '' && model != '' && state != '') {
+        return CarModel.find({
+            Year: year,
+            Make: make,
+            Model: model,
+            State: state,
+        }).exec();
+    } else if (year != '' && make != '' && model != '') {
+        return CarModel.find({
+            Year: year,
+            Make: make,
+            Model: model,
+        }).exec();
+    } else if (year != '' && make != '' && state != '') {
+        return CarModel.find({
+            Year: year,
+            Make: make,
+            State: state,
+        }).exec();
+    } else if (year != '' && model != '' && state != '') {
+        return CarModel.find({
+            Year: year,
+            Model: model,
+            State: state,
+        }).exec();
+    } else if (make != '' && model != '' && state != '') {
+        return CarModel.find({
+            Make: year,
+            Model: model,
+            State: state,
+        }).exec();
+    } else if (model != '' && state != '') {
+        return CarModel.find({
+            Model: model,
+            State: state,
+        }).exec();
+    } else if (year != '' && state != '') {
+        return CarModel.find({
+            Year: year,
+            State: state,
+        }).exec();
+    } else if (year != '' && make != '') {
+        return CarModel.find({
+            Year: year,
+            State: state,
+        }).exec();
+    } else if (year != '' && model != '') {
+        return CarModel.find({
+            Year: year,
+            Model: model,
+        }).exec();
+    } else if (make != '' && model != '') {
+        return CarModel.find({
+            Make: make,
+            Model: model,
+        }).exec();
+    } else if (make != '' && state != '') {
+        return CarModel.find({
+            Make: make,
+            State: state,
+        }).exec();
+    } else if (year != '') {
+        return CarModel.find({
+            Year: year,
+        }).exec();
+    } else if (make != '') {
+        return CarModel.find({
+            Make: make,
+        }).exec();
+    } else if (model != '') {
+        return CarModel.find({
+            Model: model,
+        }).exec();
+    } else if (state != '') {
+        return CarModel.find({
+            State: state,
+        }).exec();
+    } else {
+        return CarModel.find().exec();
+    }
 }
 
 function getCarsByYear(year) {
@@ -32,6 +106,7 @@ function getCarsByYear(year) {
 }
 
 module.exports = {
+
     createCar,
     getAllCars,
     deleteCar,
