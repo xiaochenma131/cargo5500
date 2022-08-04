c51adf128415c7f55303b2256e6ed8d70c4817b3
xiaochenma1996 <ma.xiaoch@northeastern.edu>
2022-08-03 22:06:41 -0700

    Adding reply message functionality

--- V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/routes/messages.js
+++ V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/routes/messages.js
@@ -3,6 +3,26 @@ const MessageModel = require('./model/message.model')
 
 const router = express.Router()
 
+router.post('/', function (request, response) {
+    const CarId = request.body.CarId;
+    const FromId = request.body.FromId;
+    const ToId = request.body.ToId;
+    const Content = request.body.newMessageInput;
+    message = {
+        CarId: CarId,
+        FromId: FromId,
+        ToId: ToId,
+        Content: Content,
+    }
+    return MessageModel.createMessage(message)
+        .then(response => {
+            response.status(200).send(message);
+        })
+        .catch(err => {
+            response.status(400).send(err)
+        })
+});
+
 router.post('/myMessages', function (request, response) {
     const email = request.body.email;
     return MessageModel.getMessageByToId(email)
--- V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/src/components/MessageCard.jsx
+++ V2.0/Yafei-Wang-And-Yuan-Yao-Project3-main 2/src/components/MessageCard.jsx
@@ -1,8 +1,29 @@
-import React from "react";
-import { Card, Button } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { Card, Button, Form } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 export default function MessageCard(props) {
     const message = props.message;
+    const CarId = message.CarId;
+    const FromId = message.FromId;
+    const ToId = message.ToId;
+    const ReplyFromId = ToId;
+    const ReplyToId = FromId;
+    const [newMessageInput, setNewMessageInput] = useState('');
+
+    function createReplyMessage() {
+
+        Axios.post('/api/messages', {
+            CarId: CarId,
+            FromId: ReplyFromId,
+            ToId: ReplyToId,
+            newMessageInput: newMessageInput,
+        }).then(() => { })
+            .catch(function (err) {
+                console.log(err);
+            })
+    }
 
     return (
         <div className="car-card">
@@ -13,8 +34,21 @@ export default function MessageCard(props) {
                     <Card.Text>From: {message.FromId}</Card.Text>
                     <Card.Text>Send Time: {message.createdAt}</Card.Text>
                     <Card.Text>Content: {message.Content}</Card.Text>
+                    <div className="reply-card">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Enter your message</Form.Label>
+                            <Form.Control placeholder="Enter the message"
+                                value={newMessageInput}
+                                onChange={e => setNewMessageInput(e.target.value)} />
+                        </Form.Group>
+                        <Button size="sm" className="custom-btn" onClick={createReplyMessage} as={Link} to={'/'} >
+                            Reply
+                        </Button>
+                        <div>
+                        </div>
+                    </div>
                 </Card.Body>
             </Card>
-        </div>
+        </div >
     )
 }
